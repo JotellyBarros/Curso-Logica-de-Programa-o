95b73c77268f021c3f35f718454597b16123ef32
JotellyBarros <jotelly@gmail.com>
2020-04-24 03:22:34 +0000

    Ola mundo

--- main.c
+++ main.c
@@ -1,31 +1,11 @@
 /*
  * main.c
- *
- * Copyright 2020 Jotelly Barros <jotellybarros@jotelly-speed>
- *
+ * Copyright 2020 Jotelly Barros
  */
 
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-  char nome[30];
-  char sexo;
-  int idade;
-
-  printf("Informe seu nome!\n");
-  scanf("%s", nome);  // recebe um inteiro e armazena na variável nome.
-
-  printf("Informe sua idade!\n");
-  scanf("%d", &idade);  // recebe um inteiro e armazena na variável idade.
-
-  printf("Informe seu sexo: ");
-  scanf(" %c", &sexo);  // recebe um inteiro e armazena na variável sexo.
-
-  // ----------------------------------- //
-  printf("\nSeu nome he: %s\n", nome);
-  printf("Seu sexo he: %c\n", sexo);
-  printf("Sua idade he: %d\n", idade);
-
-  // puts(nome);
+  printf("Ola mundo.\n");
   return 0;
 }
